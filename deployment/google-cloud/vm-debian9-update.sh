f365032d1899d55148cabcf719c73a9b38616699
Nicolae Carabut <nicolae@robotxlab.ai>
2018-08-07 22:36:55 -0700

    DG-741- Document and Update the scripts to deploy scandis and mobile wallet and point an existing env to staging

--- deployment/google-cloud/vm-debian9-update.sh
+++ deployment/google-cloud/vm-debian9-update.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # Fetch DisGo
+rm -rf $GOPATH/src/github.com/dispatchlabs
 mkdir -p $GOPATH/src/github.com/dispatchlabs
 cd $GOPATH/src/github.com/dispatchlabs
 git clone -b $1 https://github.com/dispatchlabs/disgo.git
@@ -16,3 +17,4 @@ go build
 sudo mkdir -p /go-binaries/config
 sudo systemctl stop dispatch-disgo-node
 sudo mv ./disgo /go-binaries/
+sudo systemctl start dispatch-disgo-node
