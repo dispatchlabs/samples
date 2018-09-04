7f0d22fbbd359e48ab84418e02672d61e1320161
Nicolae Carabut <nicolae@robotxlab.ai>
2018-09-04 14:45:11 -0700

    DG-701 - samples

--- dvm/tests/complex-contracts/contracts_from_contracts_test.go
+++ dvm/tests/complex-contracts/contracts_from_contracts_test.go
@@ -2,6 +2,7 @@ package complex_contracts
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -10,34 +11,31 @@ import (
 	"testing"
 	"time"
 
-	"github.com/dispatchlabs/disgo/bootstrap"
-	"github.com/dispatchlabs/disgo/commons/services"
 	"github.com/dispatchlabs/disgo/commons/types"
 	"github.com/dispatchlabs/disgo/commons/utils"
-	"github.com/dispatchlabs/disgo/dapos"
-	"github.com/dispatchlabs/disgo/disgover"
-	"github.com/dispatchlabs/disgo/dvm"
 )
 
-var nrOfServices = 6
+var nrOfServices = 0
 var delegateUrl = "http://127.0.0.1:1175/v1"
 
 func Test_ContractPublishAndExecuteSimpleContract(t *testing.T) {
-	utils.InitMainPackagePath()
-	utils.InitializeLogger()
+	// utils.InitMainPackagePath()
+	// utils.InitializeLogger()
 
-	utils.Events().On(services.Events.DbServiceInitFinished, allServicesInitFinished)
-	utils.Events().On(services.Events.GrpcServiceInitFinished, allServicesInitFinished)
-	utils.Events().On(services.Events.HttpServiceInitFinished, allServicesInitFinished)
+	// utils.Events().On(services.Events.DbServiceInitFinished, allServicesInitFinished)
+	// utils.Events().On(services.Events.GrpcServiceInitFinished, allServicesInitFinished)
+	// utils.Events().On(services.Events.HttpServiceInitFinished, allServicesInitFinished)
 
-	utils.Events().On(disgover.Events.DisGoverServiceInitFinished, allServicesInitFinished)
-	utils.Events().On(dapos.Events.DAPoSServiceInitFinished, allServicesInitFinished)
-	utils.Events().On(dvm.Events.DVMServiceInitFinished, allServicesInitFinished)
+	// utils.Events().On(disgover.Events.DisGoverServiceInitFinished, allServicesInitFinished)
+	// utils.Events().On(dapos.Events.DAPoSServiceInitFinished, allServicesInitFinished)
+	// utils.Events().On(dvm.Events.DVMServiceInitFinished, allServicesInitFinished)
 
-	utils.Info(fmt.Sprintf("NR of services left to be started: %d", nrOfServices))
+	// utils.Info(fmt.Sprintf("NR of services left to be started: %d", nrOfServices))
 
-	server := bootstrap.NewServer()
-	server.Go()
+	// server := bootstrap.NewServer()
+	// server.Go()
+
+	allServicesInitFinished()
 }
 
 func allServicesInitFinished() {
@@ -50,39 +48,42 @@ func allServicesInitFinished() {
 
 	const timeout = 1
 
-	go func() {
-		time.Sleep(timeout * time.Second)
-		tx := deployContract()
+	// go func() {
+	// time.Sleep(timeout * time.Second)
+	// tx := deployContract()
 
-		time.Sleep(3 * time.Second)
-		deployRcpt := getReceipt(tx.Hash, fmt.Sprintf("%s/receipts", delegateUrl))
+	// time.Sleep(3 * time.Second)
+	// deployRcpt := getReceipt(tx.Hash, fmt.Sprintf("%s/receipts", delegateUrl))
 
-		fmt.Println(fmt.Sprintf("CONTRACT-ADDRESSP: %s", deployRcpt.ContractAddress))
-	}()
+	// fmt.Println(fmt.Sprintf("CONTRACT-ADDRESS: %s", deployRcpt.ContractAddress))
+	// }()
 
-	//go func() {
-	//	time.Sleep(timeout * time.Second)
-	//	executeMethod_set()
-	//}()
+	// ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~
 
 	// go func() {
-	// 	time.Sleep(timeout * time.Second)
-	// 	executeMethod_get()
+	time.Sleep(timeout * time.Second)
+	tx := executeMethod()
+
+	time.Sleep(3 * time.Second)
+	executeRcpt := getReceipt(tx.Hash, fmt.Sprintf("%s/receipts", delegateUrl))
+
+	fmt.Println(fmt.Sprintf("CALL-RECEIPT: %v", executeRcpt))
+
+	time.Sleep(120 * time.Second)
 	// }()
 }
 
 func deployContract() *types.Transaction {
 	var privateKey = "0f86ea981203b26b5b8244c8f661e30e5104555068a4bd168d3e3015db9bb25a"
 	var from = "3ed25f42484d517cdfc72cafb7ebc9e8baa52c2c"
-	var code = "608060405234801561001057600080fd5b5060df8061001f6000396000f3006080604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063d46300fd14604e578063ee919d50146076575b600080fd5b348015605957600080fd5b50606060a0565b6040518082815260200191505060405180910390f35b348015608157600080fd5b50609e6004803603810190808035906020019092919050505060a9565b005b60008054905090565b80600081905550505600a165627a7a723058205906547745a52855a1b22685e079cbdec04bad5d24c4c243d60837b39fb845890029"
-	var abi = getAbi()
+	var code = "608060405234801561001057600080fd5b5061025f806100206000396000f30060806040526004361061004c576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063f3189d4614610051578063f474bd9e146100a8575b600080fd5b34801561005d57600080fd5b50610092600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506100f5565b6040518082815260200191505060405180910390f35b3480156100b457600080fd5b506100f3600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803590602001909291905050506101a2565b005b6000808290508073ffffffffffffffffffffffffffffffffffffffff1663d46300fd6040518163ffffffff167c0100000000000000000000000000000000000000000000000000000000028152600401602060405180830381600087803b15801561015f57600080fd5b505af1158015610173573d6000803e3d6000fd5b505050506040513d602081101561018957600080fd5b8101908080519060200190929190505050915050919050565b60008290508073ffffffffffffffffffffffffffffffffffffffff1663ee919d50836040518263ffffffff167c010000000000000000000000000000000000000000000000000000000002815260040180828152602001915050600060405180830381600087803b15801561021657600080fd5b505af115801561022a573d6000803e3d6000fd5b505050505050505600a165627a7a72305820357833ed1cddada528e80a109eb86fdea4cadc72eba11ed595a73294811c87920029"
 	var theTime = utils.ToMilliSeconds(time.Now())
 
 	var tx, _ = types.NewDeployContractTransaction(
 		privateKey,
 		from,
 		code,
-		abi,
+		hex.EncodeToString([]byte(getAbi())),
 		theTime,
 	)
 
@@ -94,62 +95,65 @@ func deployContract() *types.Transaction {
 	// fmt.Println(fmt.Sprintf("%v", response))
 }
 
-func executeMethod() {
+func executeMethod() *types.Transaction {
 	// Taken from Genesis
 	var privateKey = "0f86ea981203b26b5b8244c8f661e30e5104555068a4bd168d3e3015db9bb25a"
 	var from = "3ed25f42484d517cdfc72cafb7ebc9e8baa52c2c"
-	var to = "78337c25f0c003344c1b16e5f4b5ebda07a08cf5"
+	var to = "d1ec81ee993fcf6bd719fd182453b24848b9e353"
 
 	var theTime = utils.ToMilliSeconds(time.Now())
-	var method = "getVar5"
-	var params = make([]interface{}, 0)
-	// var params = make([]interface{}, 1)
-	// params[0] = "5555"
+	var method = "setA"
+	var params = make([]interface{}, 1)
+	// params[0] = "05394c37f2f2e0c1598914a1d5181d46a6202021"
+	params[0] = 5
 
 	var tx, _ = types.NewExecuteContractTransaction(
 		privateKey,
 		from,
 		to,
-		// hex.EncodeToString([]byte(abi)),
 		method,
 		params,
 		theTime,
 	)
 
-	dapos.GetDAPoSService().Temp_ProcessTransaction(tx)
+	postTx(tx, fmt.Sprintf("%s/transactions", delegateUrl))
+
+	return tx
+
+	// dapos.GetDAPoSService().Temp_ProcessTransaction(tx)
 }
 
 func getAbi() string {
 	return `[
-		{
-			"constant": true,
-			"inputs": [],
-			"name": "getA",
-			"outputs": [
-				{
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": false,
-			"inputs": [
-				{
-					"name": "a",
-					"type": "uint256"
-				}
-			],
-			"name": "setA",
-			"outputs": [],
-			"payable": false,
-			"stateMutability": "nonpayable",
-			"type": "function"
-		}
-	]`
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getA",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "a",
+				"type": "uint256"
+			}
+		],
+		"name": "setA",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]`
 }
 
 func postTx(tx *types.Transaction, endpoint string) {
