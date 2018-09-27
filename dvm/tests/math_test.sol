919d60f819a874f86ce191b1ed22d2e899ab65f0
Brian Woods <me@brian.biz>
2018-09-27 12:42:55 -0700

    updating tests

--- dvm/tests/math_test.sol
+++ dvm/tests/math_test.sol
@@ -2,7 +2,8 @@
 // ADD
 // ADDMOD
 // AND
-// BALANCE
+// BYTE
+// CALLCODE
 // CALLDATACOPY
 // CALLDATALOAD
 // CALLDATASIZE
@@ -10,6 +11,7 @@
 // CODECOPY
 // DIV
 // DUP1
+// DUP11
 // DUP2
 // DUP3
 // DUP4
@@ -18,7 +20,6 @@
 // EQ
 // EXP
 // GT
-// INVALID
 // ISZERO
 // JUMP
 // JUMPDEST
@@ -35,17 +36,22 @@
 // OR
 // POP
 // PUSH1
-// PUSH19
+// PUSH17
 // PUSH2
 // PUSH29
 // PUSH31
 // PUSH32
 // PUSH4
+// PUSH5
 // PUSH6
 // RETURN
 // REVERT
+// SAR
 // SDIV
 // SGT
+// SHL
+// SHR
+// SIGNEXTEND
 // SLT
 // SMOD
 // STOP
@@ -54,121 +60,181 @@
 // SWAP2
 // SWAP3
 // SWAP4
-// SWAP5
 // XOR
 
 pragma solidity ^0.4.0;
 
 contract MathTest {
-				function add(uint x, uint y) public pure returns (uint) {
-								return x + y;
-				}
+  function test_add(uint x, uint y) public pure returns (uint ret) {
+    assembly {          
+      ret := add(x, y)
+    }
+  }
 
-				function sub(uint x, uint y) public pure returns (uint) {
-								return x - y;
-				}
+  function test_sub(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := sub(x, y)
+    }
+  }
 
-				function mul(uint x, uint y) public pure returns (uint) {
-								return x * y;
-				}
+  function test_mul(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := mul(x, y)
+    }
+  }
 
-				function div(uint x, uint y) public pure returns (uint) {
-								return x / y;
-				}
+  function test_div(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := div(x, y)
+    }
+  }
 
-				function sdiv(int x, int y) public pure returns (int) {
-								return x / y;
-				}
+  function test_sdiv2() public pure returns (int ret) {
+    assembly {
+      ret := sdiv(0xf1, 0xfd)
+    }
+  }
 
-				function mod(uint x, uint y) public pure returns (uint) {
-								return x % y;
-				}
+  function test_sdiv(int x, int y) public pure returns (int ret) {
+    assembly {
+      ret := sdiv(x, y)
+    }
+  }
 
-				function smod(int x, int y) public pure returns (int) {
-								return x % y;
-				}
+  function test_mod(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := mod(x, y)
+    }
+  }
 
-				function exp(uint x, uint y) public pure returns (uint) {
-								return x ** y;	
-				}
+  function test_smod(int x, int y) public pure returns (int ret) {
+    assembly {
+      ret := smod(x, y)
+    }
+  }
 
-				function not(bool x) public pure returns (bool) {
-								return !x;
-				}
+  function test_exp(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := exp(x, y)
+    }
+  }
 
-				function lt(uint x, uint y) public pure returns (bool) {
-								return x < y;
-				}
+  function test_not(bool x) public pure returns (bool ret) {
+    assembly {
+      ret := not(x)
+    }
+  }
 
-				function gt(uint x, uint y) public pure returns (bool) {
-								return x > y;
-				}
+  function test_lt(uint x, uint y) public pure returns (bool ret) {
+    assembly {
+      ret := lt(x, y)
+    }
+  }
 
-				function slt(int x, int y) public pure returns (bool) {
-								return x < y;
-				}
+  function test_gt(uint x, uint y) public pure returns (bool ret) {
+    assembly {
+      ret := gt(x, y)
+    }
+  }
 
-				function sgt(int x, int y) public pure returns (bool) {
-								return x > y;
-				}
+  function test_slt(int x, int y) public pure returns (bool ret) {
+    assembly {
+      ret := slt(x, y)
+    }
+  }
 
-				function eq(uint x, uint y) public pure returns (bool) {
-								return x == y;
-				}
+  function test_sgt(int x, int y) public pure returns (bool ret) {
+    assembly {
+      ret := sgt(x, y)
+    }
+  }
 
-				function iszero(uint x) public pure returns (bool) {
-								return x == 0;
-				}
+  function test_eq(uint x, uint y) public pure returns (bool ret) {
+    assembly {
+      ret := eq(x, y)
+    }
+  }
 
-				function and(uint x, uint y) public pure returns (uint) {
-								return x & y;
-				}
+  function test_iszero(uint x) public pure returns (bool ret) {
+    assembly {
+      ret := iszero(x)
+    }
+  }
 
-				function or(uint x, uint y) public pure returns (uint) {
-								return x | y;
-				}
+  function test_and(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := and(x, y)
+    }
+  }
 
-				function xor(uint x, uint y) public pure returns (uint) {
-								return x ^ y;
-				}
+  function test_or(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := or(x, y)
+    }
+  }
 
-				/* This doesn't trigger the opcode */
-				// function get_byte(bytes b) public pure returns (byte) {
-				// 				return b[4];
-				// }
+  function test_xor(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := xor(x, y)
+    }
+  }
 
-				function shl(uint x, uint y) public pure returns (uint) {
-								return x << y;				
-				}
+  function test_byte(uint256 index) public pure returns (bytes32 ret) {
+    assembly {
+      ret := byte(index, "123456789foobarfoo")
+    }
+  }
 
-				function shr(uint x, uint y) public pure returns (uint) {
-								return x >> y;
-				}
+  function test_shl(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := shl(x, y)
+    }
+  }
 
-				function sar(int x, int y) public pure returns (int) {
-								return x >> y;							
-				}
+  function test_shr(uint x, uint y) public pure returns (uint ret) {
+    assembly {
+      ret := shr(x, y)
+    }
+  }
 
-				function addmod_test(uint x, uint y, uint m) public pure returns (uint) {
-								return addmod(x, y, m);
-				}
+  function test_sar(int x, int y) public pure returns (int ret) {
+    assembly {
+      ret := sar(x, y)
+    }
+  }
 
-				function mulmod_test(uint x, uint y, uint m) public pure returns (uint) {
-								return mulmod(x, y, m);
-				}
+  function test_addmod(uint x, uint y, uint m) public pure returns (uint ret) {
+    assembly {
+      ret := addmod(x, y, m)
+    }
+  }
 
-				/* this doesn't generate the right op code for some reason */
-				function signextend(int i, int x) public pure returns (int) {
-								x >> i;
-				} 
+  function test_mulmod(uint x, uint y, uint m) public pure returns (uint ret) {
+    assembly {
+      ret := mulmod(x, y, m)
+    }
+  }
 
-				/* this is automatically executed when the contract is run
-				function keccak256(uint p, uint n) public pure returns (uint) {
-								_;
-				} */
+  function test_signextend(int32 original) public pure returns (bytes32) {
+    assembly {
+      mstore(0, signextend(0x10, original))
+      return(0, 0x64)
+    }
+  }
 
-				/* this gets converted to keccak256 automatically */
-				function test_sha3(bytes b) public pure returns (bytes32) {
-								return sha3(b);
-				}
+  function test_keccak256() public pure returns (bytes32 ret) {
+    assembly {
+      mstore(0, "this is a test")
+      ret := keccak256(0, 0x0e)
+    }
+  } 
+
+  /* this gets converted to keccak256 automatically however in the EVM and DVM
+  * it's calles opSha3 (inconsistent) */
+  function test_sha3() public pure returns (bytes32 ret) {
+    assembly {
+      mstore(0, "this is a test")
+      ret := keccak256(0, 0x0e)
+    }
+  }
 }
